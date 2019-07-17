8f64fed5a52e841bcf8b04d1ae114b10242a330e
MaldoCarre <38377990+MaldoCarre@users.noreply.github.com>
2019-07-17 18:08:46 -0300

    Add files via upload

--- core/views.py
+++ core/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from.forms import estudioFormu,pacienteFormu
-=======
-from.forms import estudioFormu
->>>>>>> 7927276fd1b2c4dd6dc819fd275d3e6d654cb1e2
+from.models import Pacientes
+from django.views.generic import ListView
 # Create your views here
 #en esta parte se generan las vistas para direccionar nuestra app 
 def fomPaciente(request):
+    if request.method == 'GET':
+        q = request.GET.get('q', '')
+        eventos = Pacientes.objects.filter(nombre__icontains=q)
+        return render(request, 'core/formPaciente.html', {'eventos': eventos})
+
     if request.method == 'POST':
         form=estudioFormu(request.POST) #indico metodos POST de la transmicion de mis datos
         if form.is_valid(): #pregunto si mis datos son validos
@@ -19,7 +22,6 @@ def fomPaciente(request):
 def home(request):
     return render(request,"core/home.html",context=None) #funcion de la vista home 
 
-<<<<<<< HEAD
 #carga del formulario de los pacientes.
 
 def cargaPacientes(request):
@@ -31,10 +33,11 @@ def cargaPacientes(request):
     else:
         form=pacienteFormu
     return render (request,"core/cargapaciente.html",{'form':form})
-    
+
+class Listapacientes(ListView):
+    model = Pacientes
+    template_name = 'core/listapacientes.html'
 
 
 
-=======
->>>>>>> 7927276fd1b2c4dd6dc819fd275d3e6d654cb1e2
 
